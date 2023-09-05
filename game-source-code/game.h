f5716dc7376fe82251c80539a9924e99f9731469
Danielbyte <danielmasango7@gmail.com>
2023-09-05 13:05:49 +0200

    Initialization

---
+++ game-source-code/game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+class Game
+{
+public:
+	Game();
+	void run();
+private:
+};
+#endif
+
