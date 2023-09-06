bfa22c849048b94073902043b90b659a110b178a
Danielbyte <danielmasango7@gmail.com>
2023-09-06 11:10:11 +0200

    Loaded player ship sprites

--- game-source-code/spriteStore.cpp
+++ game-source-code/spriteStore.cpp
@@ -1,3 +1,18 @@
 #include "spriteStore.h"
 
-SpriteStore::SpriteStore(){}
+SpriteStore::SpriteStore()
+{
+	loadPlayerShipSprites();
+}
+
+void SpriteStore::loadPlayerShipSprites()
+{
+	sf::Vector2f spaceShipDimensions;
+	spaceShipDimensions.x = 50.0f;
+	spaceShipDimensions.y = 20.0f;
+	playerShip_lT.loadFromFile("resources/spaceship-left.png");
+	playerShip_rT.loadFromFile("resources/spaceship-right.png");
+
+	playerShip_lS.setTexture(playerShip_lT);
+	playerShip_lS.setOrigin(spaceShipDimensions / 2.0f);
+}
--- game-source-code/spriteStore.h
+++ game-source-code/spriteStore.h
@@ -3,13 +3,20 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
+
 class SpriteStore
 {
 public:
 	SpriteStore();
+	void updatePlayerShipSprite(const bool& shipDirection);
+	sf::Sprite getPlayerShipSprite() const;
+	void loadPlayerShipSprites();
 
 private:
-
+	sf::Texture playerShip_lT; // player ship facing left(texture)
+	sf::Sprite playerShip_lS; // player ship facing left(sprite)
+	sf::Texture playerShip_rT; // player ship facing right(texture)
+	sf::Sprite playerShip_rS; // player ship facing right(sprite)
 };
 #endif // !SPRITESTORE_H
 
