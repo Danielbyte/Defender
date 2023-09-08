fad9bb830a30cb86918ccfe79327e5dd1569b268
Danielbyte <danielmasango7@gmail.com>
2023-09-08 09:18:51 +0200

    added function prototypes and variables in stopWatch class

--- game-source-code/stopWatch.h
+++ game-source-code/stopWatch.h
@@ -1,13 +1,20 @@
 #ifndef STOPWATCH_H
 #define STOPWATCH_H
 
+#include <ctime>
+
 class StopWatch
 {
 public:
 	StopWatch();
+	float time_elapsed();
+	void stop();
+	void restart();
 
 private:
-
+	float current_time();
+	float initial_time;
+	float final_time;
 };
 #endif // !STOPWATCH_H
 
