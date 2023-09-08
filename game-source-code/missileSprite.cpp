9aadea93d65b5f952899e53b9ada2de548945022
Danielbyte <danielmasango7@gmail.com>
2023-09-08 23:27:21 +0200

    Added functionality to update missile textures

--- game-source-code/missileSprite.cpp
+++ game-source-code/missileSprite.cpp
@@ -1,3 +1,34 @@
 #include "missileSprite.h"
 
-MissileSprite::MissileSprite(){}
+MissileSprite::MissileSprite():
+	sprite_period{0.2f}
+{
+	sf::Vector2f dimensions;
+	dimensions.x = 6.0f;
+	dimensions.y = 6.0f;
+	loadTextures();
+	InitialiseEntityOrigin(dimensions);
+}
+
+void MissileSprite::loadTextures()
+{
+	missile1_t.loadFromFile("resources/missile1.png");
+	missile2_t.loadFromFile("resources/missile2.png");
+}
+
+void MissileSprite::setTexture(std::shared_ptr<StopWatch>& watch)
+{
+	auto _time = watch->time_elapsed();
+
+	if (_time == 0 && _time <= sprite_period)
+	{
+		updateSpriteTexture(missile1_t);
+		return;
+	}
+
+	if (_time > sprite_period && _time <= 2 * sprite_period)
+	{
+		updateSpriteTexture(missile2_t);
+		watch->restart();
+	}
+}
--- game-source-code/missileSprite.h
+++ game-source-code/missileSprite.h
@@ -1,13 +1,20 @@
 #ifndef MISSILESPRITE_H
 #define MISSILESPRITE_H
 
-#include"spriteStore.h"
+#include "spriteStore.h"
+#include "stopWatch.h"
 
 class MissileSprite : public SpriteStore
 {
 public:
 	MissileSprite();
+	void virtual loadTextures();
+	void setTexture(std::shared_ptr<StopWatch>& watch);
+
 private:
+	sf::Texture missile1_t;
+	sf::Texture missile2_t;
+	float sprite_period; //time witch a sprite should display
 
 };
 #endif // !MISSILESPRITE_H
