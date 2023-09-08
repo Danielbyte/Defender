a45a1f187ca97e97fabff4c0a98d2c33065af992
Danielbyte <danielmasango7@gmail.com>
2023-09-08 12:57:20 +0200

    Added function prototypes and member variables for gameWorld class

--- game-source-code/gameWorld.h
+++ game-source-code/gameWorld.h
@@ -1,13 +1,20 @@
 #ifndef GAMEWORLD_H
 #define GAMEWORLD_H
 
+#include "lander.h"
+
+enum class Enemy{Lander,Bomber,Pod,None};
+
 class GameWorld
 {
 public:
 	GameWorld();
+	void updateGameWorld(std::vector<std::shared_ptr<Lander>>& lander_objects,
+		std::vector<std::shared_ptr<LanderSprite>>& lander_object_sprites);
 
 private:
-
+	std::shared_ptr<StopWatch> world_watch = std::make_shared<StopWatch>();
+	Enemy enemy;
 };
 #endif // !GAMEWORLD_H
 
