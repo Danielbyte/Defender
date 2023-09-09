d1d0ed0e1f043de8594f082b48301733b48638d8
Danielbyte <danielmasango7@gmail.com>
2023-09-09 21:33:47 +0200

    Added playerLanderCollisions protoype function in collisionsManager class

--- game-source-code/collisionsManager.cpp
+++ game-source-code/collisionsManager.cpp
@@ -1,3 +1,9 @@
 #include "collisionsManager.h"
 
 CollisionsManager::CollisionsManager(){}
+
+void playerLanderCollisions(std::shared_ptr<Player>& player, std::vector<std::shared_ptr<Lander>>& landers,
+	std::vector<std::shared_ptr<LanderSprite>>& lander_sprites)
+{
+
+}
--- game-source-code/collisionsManager.h
+++ game-source-code/collisionsManager.h
@@ -1,10 +1,17 @@
 #ifndef COLLISIONSMANAGER_H
 #define COLLISIONSMANAGER_H
 
+#include "collisions.h"
+#include "player.h"
+#include "lander.h"
+
 class CollisionsManager
 {
 public:
 	CollisionsManager();
+	void playerLanderCollisions(std::shared_ptr<Player>& player, std::vector<std::shared_ptr<Lander>>& landers,
+		std::vector<std::shared_ptr<LanderSprite>>& lander_sprites);
+
 
 private:
 
