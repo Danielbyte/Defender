859a54a53ff5a8b685e66aca1a235021ecffc256
Danielbyte <danielmasango7@gmail.com>
2023-09-09 18:44:51 +0200

    Added checkCollision function in collision class

--- game-source-code/collisions.cpp
+++ game-source-code/collisions.cpp
@@ -1,3 +1,9 @@
 #include "collisions.h"
 
 Collisions::Collisions() {}
+
+bool Collisions::checkCollision(float obj1Xposition, float obj1Yposition, float obj1Width, float obj1Length,
+	float obj2Xposition, float obj2Yposition, float obj2Width, float obj2Length)
+{
+
+}
--- game-source-code/collisions.h
+++ game-source-code/collisions.h
@@ -5,6 +5,8 @@ class Collisions
 {
 public:
 	Collisions();
+	bool checkCollision(float obj1Xposition, float obj1Yposition, float obj1Width, float obj1Length,
+		float obj2Xposition,float obj2Yposition, float obj2Width, float obj2Length);
 
 private:
 
