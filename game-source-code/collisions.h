7b8b2a63851439ed9c2281c1578881a25640b563
Danielbyte <danielmasango7@gmail.com>
2023-09-09 14:11:01 +0200

    Added collisions and collisionManager classes

---
+++ game-source-code/collisions.h
@@ -0,0 +1,13 @@
+#ifndef COLLISIONS_H
+#define COLLISIONS_H
+
+class Collisions
+{
+public:
+	Collisions();
+
+private:
+
+};
+#endif // !COLLISIONS_H
+
---
+++ game-source-code/collisionsManager.h
@@ -0,0 +1,13 @@
+#ifndef COLLISIONSMANAGER_H
+#define COLLISIONSMANAGER_H
+
+class CollisionsManager
+{
+public:
+	CollisionsManager();
+
+private:
+
+};
+#endif // !COLLISIONSMANAGER_H
+
