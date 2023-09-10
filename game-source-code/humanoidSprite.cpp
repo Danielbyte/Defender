90e4053b0ac73422860dfbe4754e4da6e6f568aa
Danielbyte <danielmasango7@gmail.com>
2023-09-11 00:54:31 +0200

    Added HumanoidSprite class

---
+++ game-source-code/humanoidSprite.cpp
@@ -0,0 +1,3 @@
+#include "humanoidSprite.h"
+
+HumanoidSprite::HumanoidSprite(){}
---
+++ game-source-code/humanoidSprite.h
@@ -0,0 +1,15 @@
+#ifndef HUMANOIDSPRITE_H
+#define HUMANOIDSPRITE_H
+
+#include "spriteStore.h"
+
+class HumanoidSprite
+{
+public:
+	HumanoidSprite();
+
+private:
+
+};
+#endif // !HUMANOIDSPRITE_H
+
