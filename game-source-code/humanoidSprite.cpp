0ff31ecd78ed5be5dee068d8d6c4a6a5a89d5adb
Danielbyte <danielmasango7@gmail.com>
2023-09-11 01:14:34 +0200

    Defined a function to load humanoid textures into memory

--- game-source-code/humanoidSprite.cpp
+++ game-source-code/humanoidSprite.cpp
@@ -4,7 +4,13 @@ HumanoidSprite::HumanoidSprite(){}
 
 void HumanoidSprite::loadTextures()
 {
-
+	humanoidLeft1_t.loadFromFile("resources/humanoidleft-1.png");
+	humanoidLeft2_t.loadFromFile("resources/humanoidleft-2.png");
+	humanoidRight1_t.loadFromFile("resources/humanoidright-1.png");
+	humanoidRight2_t.loadFromFile("resources/humanoidright-2.png");
+	fallingHumanoid1_t.loadFromFile("resources/falling-humanoid1.png");
+	fallingHumanoid2_t.loadFromFile("resources/falling-humanoid2.png");
+	fallingHumanoid3_t.loadFromFile("resources/falling-humanoid3.png");
 }
 
 void HumanoidSprite::setTexture()
