20c625f3d934ff86428474a8a0c4be3e0e321a90
Danielbyte <danielmasango7@gmail.com>
2023-09-27 22:08:57 +0200

    Test case: test if timer can be stooped

--- test-source-code/tests.cpp
+++ test-source-code/tests.cpp
@@ -24,3 +24,25 @@ TEST_CASE("Test if stop watch can function as a timer")
 	//initial time should be lesser than final time
 	DOCTEST_CHECK_LE(initial_time, final_time);
 }
+
+TEST_CASE("Test if timer can be stopped")
+{
+	std::shared_ptr<StopWatch>stop_watch = std::make_shared<StopWatch>();
+	auto initial_time = stop_watch->time_elapsed();
+	//Pass some time
+	while (stop_watch->time_elapsed() < 0.1f)
+	{
+		//do nothing
+	}
+	//stop the watch
+	stop_watch->stop();
+
+	auto final_time1 = stop_watch->time_elapsed();
+	auto final_time2 = stop_watch->time_elapsed();
+
+	//final_time1 and final_time2 should be equal
+	DOCTEST_CHECK_EQ(final_time1, final_time2);
+	//final_time1 and final_time2 should be greater than initial time
+	DOCTEST_CHECK_GT(final_time1, initial_time);
+	DOCTEST_CHECK_GT(final_time2, initial_time);
+}
