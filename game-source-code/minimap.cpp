57faec5dd301d157b4d51c0e1d318da14f9c856c
Danielbyte <danielmasango7@gmail.com>
2023-10-01 10:54:14 +0200

    Mini-map: migrated minimap variables from game class to minimap class

---
+++ game-source-code/minimap.cpp
@@ -0,0 +1,8 @@
+#include "minimap.h"
+
+Minimap::Minimap(){}
+
+void Minimap::updateCamera(std::shared_ptr<Player>& player, std::shared_ptr<sf::RenderWindow>& window)
+{
+
+}
--- game-source-code/minimap.h
+++ game-source-code/minimap.h
@@ -13,8 +13,28 @@ class Minimap
 {
 public:
 	Minimap();
-private:
+	void updateCamera(std::shared_ptr<Player>& player, std::shared_ptr<sf::RenderWindow>& window);
 
+private:
+	//Mini-map views
+	sf::View miniMapView;
+	sf::Vector2f miniMapViewSize;
+
+	sf::Vector2f miniMapPosition;
+	sf::Texture miniMap_t;
+	sf::Sprite miniMap_s;
+
+	sf::Texture miniMainSection_t;
+	sf::Sprite miniMainSection_s;
+	sf::Vector2f gameSectionPosition;
+
+
+	sf::Texture miniTerrain1_t;
+	sf::Vector2f t1Pos;
+	sf::Sprite miniTerrain1_s;
+	sf::Texture miniTerrain2_t;
+	sf::Sprite miniTerrain2_s;
+	sf::Vector2f t2Pos;
 };
 #endif // !MINIMAP_H
 
