6304eebc2c0f9210b061bd2f52dab19e9701b3c4
Danielbyte <danielmasango7@gmail.com>
2023-10-01 10:40:32 +0200

    Added minimap classes for better abstraction

---
+++ game-source-code/minimap.h
@@ -0,0 +1,20 @@
+#ifndef MINIMAP_H
+#define MINIMAP_H
+
+#include <SFML/Graphics.hpp>
+#include "player.h"
+#include "playerSprite.h"
+#include "lander.h"
+#include "landerSprite.h"
+#include "humanoid.h"
+#include "humanoidSprite.h"
+
+class Minimap
+{
+public:
+	Minimap();
+private:
+
+};
+#endif // !MINIMAP_H
+
