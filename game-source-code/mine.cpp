0612106a3ebe10a3b598393bfec495facffcb42d
Danielbyte <danielmasango7@gmail.com>
2023-10-09 00:36:53 +0200

    Bombers: Added function to update mines

--- game-source-code/mine.cpp
+++ game-source-code/mine.cpp
@@ -1,3 +1,23 @@
 #include "mine.h"
 
-Mine::Mine(){}
+Mine::Mine():
+	xPosition{-100.0f},
+	yPosition{-100.0f}
+{}
+
+Mine::Mine(float _xPosition, float _yPosition):
+	xPosition{_xPosition},
+	yPosition{_yPosition}
+{
+	lifeTime->restart();
+}
+
+std::tuple<float, float> Mine::getPosition() const
+{
+	return { xPosition, yPosition };
+}
+
+void Mine::updateMine(std::vector<std::shared_ptr<Mine>>& mines, std::vector<std::shared_ptr<MineSprite>>& mine_sprites)
+{
+
+}
--- game-source-code/mine.h
+++ game-source-code/mine.h
@@ -1,13 +1,20 @@
 #ifndef MINE_H
 #define MINE_H
 
+#include "mineSprite.h"
+
 class Mine
 {
 public:
 	Mine();
-	void placeMine();
-private:
+	Mine(float _xPosition, float _yPosition);
+	std::tuple<float, float> getPosition() const;
+	void updateMine(std::vector<std::shared_ptr<Mine>>& mines, std::vector<std::shared_ptr<MineSprite>>& mine_sprites);
 
+private:
+	float xPosition;
+	float yPosition;
+	std::shared_ptr<StopWatch>lifeTime = std::make_shared<StopWatch>();
 };
 #endif // !MINE_H
 
