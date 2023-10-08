962d190b68c91181237dc60e7f4bb61557b20fbe
Danielbyte <danielmasango7@gmail.com>
2023-10-08 09:09:50 +0200

    Bombers: added update function

--- game-source-code/bombers.cpp
+++ game-source-code/bombers.cpp
@@ -1,12 +1,16 @@
 #include "bombers.h"
 Bombers::Bombers():
 	xPosition{-5.0f},
-	yPosition{-5.0f}
+	yPosition{-5.0f},
+	moveLeft{false},
+	moveRight{false}
 {}
 
 Bombers::Bombers(std::shared_ptr<Player>& player):
 	xPosition{-5.0f},
-	yPosition{-5.0f}
+	yPosition{-5.0f},
+	moveLeft{false},
+	moveRight{false}
 {
 	auto [xPlayerPos, yPlayerPos] = player->getPlayerPosition();
 	auto playerDirection = player->getDirection();
@@ -16,12 +20,24 @@ Bombers::Bombers(std::shared_ptr<Player>& player):
 void Bombers::spawn(float playerXposition, float playerYposition, std::string playerDirection)
 {
 	if (playerDirection == "left")
+	{
 		xPosition = playerXposition - 200.0f;
+		moveRight = true;
+	}
+		
 
 	if (playerDirection == "right")
+	{
 		xPosition = playerXposition + 200.0f;
-
+		moveLeft;
+	}
+		
 	yPosition = playerYposition;
 	if (playerYposition >= 480.0f || playerYposition <= 150.0f)
 		yPosition = 400.0f;	
+}
+
+void Bombers::update()
+{
+
 }
--- game-source-code/bombers.h
+++ game-source-code/bombers.h
@@ -8,10 +8,13 @@ class Bombers : public Shooter
 public:
 	Bombers();
 	Bombers(std::shared_ptr<Player>& player);
+	void update();
 
 private:
 	float xPosition;
 	float yPosition;
+	bool moveLeft;
+	bool moveRight;
 
 	void spawn(float playerXposition, float playerYposition, std::string playerDirection);
 };
