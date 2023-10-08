7bf8913bf84c0836f4d04b4c2dccfff4ea9cd080
Danielbyte <danielmasango7@gmail.com>
2023-10-08 09:05:14 +0200

    Bomber: Added spawning position

--- game-source-code/bombers.cpp
+++ game-source-code/bombers.cpp
@@ -1,3 +1,27 @@
 #include "bombers.h"
+Bombers::Bombers():
+	xPosition{-5.0f},
+	yPosition{-5.0f}
+{}
 
-Bombers::Bombers(){}
+Bombers::Bombers(std::shared_ptr<Player>& player):
+	xPosition{-5.0f},
+	yPosition{-5.0f}
+{
+	auto [xPlayerPos, yPlayerPos] = player->getPlayerPosition();
+	auto playerDirection = player->getDirection();
+	spawn(xPlayerPos, yPlayerPos, playerDirection);
+}
+
+void Bombers::spawn(float playerXposition, float playerYposition, std::string playerDirection)
+{
+	if (playerDirection == "left")
+		xPosition = playerXposition - 200.0f;
+
+	if (playerDirection == "right")
+		xPosition = playerXposition + 200.0f;
+
+	yPosition = playerYposition;
+	if (playerYposition >= 480.0f || playerYposition <= 150.0f)
+		yPosition = 400.0f;	
+}
--- game-source-code/bombers.h
+++ game-source-code/bombers.h
@@ -1,13 +1,19 @@
 #ifndef BOMBERS_H
 #define BOMBERS_H
 #include "shooter.h"
+#include "player.h"
 
 class Bombers : public Shooter
 {
 public:
 	Bombers();
-private:
+	Bombers(std::shared_ptr<Player>& player);
 
+private:
+	float xPosition;
+	float yPosition;
+
+	void spawn(float playerXposition, float playerYposition, std::string playerDirection);
 };
 #endif // !BOMBERS_H
 
