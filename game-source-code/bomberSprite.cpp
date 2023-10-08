41926652f125cbce779e27c47158c38e9cae9f1b
Danielbyte <danielmasango7@gmail.com>
2023-10-08 10:44:16 +0200

    Bombers: Added Bomber textures

--- game-source-code/bomberSprite.cpp
+++ game-source-code/bomberSprite.cpp
@@ -1,3 +1,14 @@
 #include "bomberSprite.h"
 
 BomberSprite::BomberSprite(){}
+
+void BomberSprite::loadTextures()
+{
+	bomber1_t.loadFromFile("resources/bomber1.png");
+	bomber2_t.loadFromFile("resources/bomber2.png");
+	bomber3_t.loadFromFile("resources/bomber3.png");
+	bomber4_t.loadFromFile("resources/bomber4.png");
+	bomber5_t.loadFromFile("resources/bomber5.png");
+	bomber6_t.loadFromFile("resources/bomber6.png");
+	bomber7_t.loadFromFile("resources/bomber7.png");
+}
--- game-source-code/bomberSprite.h
+++ game-source-code/bomberSprite.h
@@ -7,9 +7,16 @@ class BomberSprite : public SpriteStore
 {
 public:
 	BomberSprite();
+	void virtual loadTextures();
 
 private:
-
+	sf::Texture bomber1_t;
+	sf::Texture bomber2_t;
+	sf::Texture bomber3_t;
+	sf::Texture bomber4_t;
+	sf::Texture bomber5_t;
+	sf::Texture bomber6_t;
+	sf::Texture bomber7_t;
 };
 #endif // !BOMBERSPRITE_H
 
