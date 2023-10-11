cdb1883dfe832ba9527f53a5fdf717bb1f4a0d98
Danielbyte <danielmasango7@gmail.com>
2023-10-11 09:11:56 +0200

    Scores: Added logic to update high_score text file

--- game-source-code/scoreManager.cpp
+++ game-source-code/scoreManager.cpp
@@ -9,10 +9,27 @@ ScoreManager::ScoreManager():
 
 int ScoreManager::getHighScore()
 {
-
+	return highScore;
 }
 
-void ScoreManager::updateHighScore(const std::string scoreType)
+void ScoreManager::updateHighScore(const int currentScore, const std::string scoreType)
 {
+	if (currentScore < 0) throw NegativeScoreNotAllowed{};
 
+	highScoreFile.open("resources/high_score.text");
+
+	//update high score if prev high score is undertaken
+	if (highScoreFile.is_open())
+	{
+		highScoreFile >> highScore;
+		if (highScore < currentScore)
+			highScore = currentScore;
+	}
+	highScoreFile.close();
+
+	outFile.open("resources/high_score.text");
+	if (outFile.is_open())
+		outFile << highScore;
+
+	outFile.close();
 }
--- game-source-code/scoreManager.h
+++ game-source-code/scoreManager.h
@@ -11,13 +11,17 @@ class ScoreManager
 public:
 	ScoreManager();
 	int getHighScore();
-	void updateHighScore(const std::string scoreType);
+	void updateHighScore(const int cuurentScore, const std::string scoreType);
 
 private:
 	int highScore;
 	int landerPoints;
 	int bomberPoints;
 	int rescuePoints;
+	int currentScore;
+
+	std::ifstream highScoreFile;
+	std::ofstream outFile;
 };
 #endif // !SCOREMANAGER_H
 
