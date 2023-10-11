881df7bf9cec73da09f7671a27eb22352cb33622
Danielbyte <danielmasango7@gmail.com>
2023-10-11 08:30:25 +0200

    Scores: Added scoreManager class

---
+++ game-source-code/scoreManager.h
@@ -0,0 +1,18 @@
+#ifndef SCOREMANAGER_H
+#define SCOREMANAGER_H
+
+#include <sstream>
+#include <fstream>
+
+class NegativeScoreNotAllowed{};
+
+class ScoreManager
+{
+public:
+	ScoreManager();
+
+private:
+
+};
+#endif // !SCOREMANAGER_H
+
