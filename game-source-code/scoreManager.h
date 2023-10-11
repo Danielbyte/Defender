c04f26eaa9cf221accc6df81211f977c0f2f0c7b
Danielbyte <danielmasango7@gmail.com>
2023-10-11 08:36:18 +0200

    Scores: Added function prototypes in scoreManager class

--- game-source-code/scoreManager.h
+++ game-source-code/scoreManager.h
@@ -10,9 +10,14 @@ class ScoreManager
 {
 public:
 	ScoreManager();
+	int getHighScore();
+	void updateHighScore(const std::string scoreType);
 
 private:
-
+	int highScore;
+	int landerPoints;
+	int bomberPoints;
+	int rescuePoints;
 };
 #endif // !SCOREMANAGER_H
 
