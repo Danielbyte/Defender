04ff5ce486f781b4570d2d3a219f51c832d35582
Danielbyte <danielmasango7@gmail.com>
2023-10-11 08:41:49 +0200

    Scores: initialised score points

--- game-source-code/scoreManager.cpp
+++ game-source-code/scoreManager.cpp
@@ -1,3 +1,18 @@
 #include "scoreManager.h"
 
-ScoreManager::ScoreManager(){}
+ScoreManager::ScoreManager():
+	highScore{0},
+	landerPoints{5},
+	bomberPoints{5},
+	rescuePoints{5}
+{}
+
+int ScoreManager::getHighScore()
+{
+
+}
+
+void ScoreManager::updateHighScore(const std::string scoreType)
+{
+
+}
